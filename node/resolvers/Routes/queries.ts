01ca252fe4cccb34bfc25a1e761f8344a1343226
Rudge <rudgee@gmail.com>
2023-11-28 21:56:52 -0300

    fix: remove unnecessary context in storefront-permission query

--- node/resolvers/Routes/queries.ts
+++ node/resolvers/Routes/queries.ts
@@ -1,6 +1,6 @@
 export const QUERIES = {
   getPermission: `query permissions {
-    checkUserPermission @context(provider: "vtex.storefront-permissions"){
+    checkUserPermission {
       role {
         id
         name
